4135fe72870c425f76591ef0571a3679f2bb650f
ajpaneru <107987439+ajpaneru@users.noreply.github.com>
2024-01-20 00:37:34 -0500

    this is the first assignment1

---
+++ LSO_project_assignments_Spring_2024/src/org/howard/edu/lsp/hw1/HelloWorld1.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.hw1;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!"); 
+
+
+	}
+
+}
